ad4e740a2071fa918607778cf92995f8bb679415
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-10-27 19:55:32 +0100

    added new package

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
 app.get('/', (req,res) => {
     res.status(200);
     res.send({ "slackUsername": "Dominion", 
