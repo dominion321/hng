0b9362c47acb4191e9928d9ff2fa32480007209f
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-10-27 18:59:34 +0100

    added some content

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-    res.send({ "slackUsername": String, "backend": Boolean, "age": Number, "bio": String });
+    res.send({ "slackUsername": "Dominion", 
+    "backend": true, 
+    "age": 21, 
+    "bio": "I am an Associate Google Cloud Engineering in the making and an aspiring software engineer. " });
 })
 
 
