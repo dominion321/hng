8404e9421121ed5c7d0310fae55cd03025738406
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-10-27 19:35:20 +0100

    building to deploy on vercel

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
+    res.status(200);
     res.send({ "slackUsername": "Dominion", 
     "backend": true, 
     "age": 21, 
-    "bio": "I am an Associate Google Cloud Engineering in the making and an aspiring software engineer. " });
+    "bio": "I am an Associate Google Cloud Engineering in the making and an aspiring software engineer. " 
+    });
+    
 })
+
 
 
 app.listen(port,() => {
     console.log(`Listening at port ${port}`)
 })
+
+module.exports = app;
