326484104ec033434f01ff45934e3c066c4c06e1
dominion321 <dominion.ikpeawujor@stu.cu.edu.ng>
2022-10-28 09:03:51 +0100

    configured CORDS header

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
+
 app.get('/', (req,res) => {
     res.status(200);
     res.send({ "slackUsername": "Dominion", 
